b724b543fb001dd280f961b2a76a06f2f180dea3
Kyle Mellander <kyle@koynas.com>
2015-08-07 10:02:58 -0700

    add integration test file with test for pulling up the index page

---
+++ spec/word_count_integration_spec.rb
@@ -0,0 +1,11 @@
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe("Word Count Web Interface", {:type => :feature}) do
+  it("successfully pulls up index page") do
+    visit('/')
+    expect(page).to have_content("Word Count")
+  end
+end
