1bf714d9f5791d1b37cd26b3c8a0c576af8980db
Kyle Mellander <kyle@koynas.com>
2015-08-07 10:09:01 -0700

    Create app.rb for index display

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,6 @@
 class String
   def word_count(word = nil)
+    return 0 if !word 
     phrase_words = self.gsub(/[^A-Za-z0-9\s\']/i, '').split()
     match_result = 0
     phrase_words.each do |phrase_word|
