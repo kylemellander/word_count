0e93c728807965225e3ac2415cadbaf793547434
Kyle Mellander <kyle@koynas.com>
2015-08-07 10:37:08 -0700

    test for pulling up results page and code for form on index page

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -8,4 +8,12 @@ describe("Word Count Web Interface", {:type => :feature}) do
     visit('/')
     expect(page).to have_content("Word Count")
   end
+
+  it("successfully goes to results page if input is given") do
+    visit('/')
+    fill_in('phrase', :with => 'this is my phrase')
+    fill_in('word', :with => 'this')
+    click_button('Find')
+    expect(page).to have_content('match')
+  end
 end
