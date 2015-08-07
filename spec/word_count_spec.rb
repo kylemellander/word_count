17af853291f18adf5ceeaa96acf5fd6e4e63ee46
Kyle Mellander <kyle@koynas.com>
2015-08-07 09:31:47 -0700

    Add test for 1 response on matching the same string in both argument and object

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,8 +7,11 @@ describe("String#word_count") do
     expect("word".word_count()).to(eq(0))
   end
 
-  it("returns 0 if no words match") do
+  it("returns 0 if no words match in single word strings") do
     expect("word".word_count("new")).to(eq(0))
   end
-  
+
+  it("returns correct response when matches are found with the same object and argument") do
+    expect("word".word_count("word")).to(eq(1))s
+  end
 end
